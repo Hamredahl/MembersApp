cd2904063efd84a08b1235908cc7a899a4b7f478
Hamredahl <JHamredahl@gmail.com>
2025-06-10 11:15:10 +0200

    Members Viewmodel

---
+++ MembersApp.Web/Views/Member/MembersVM.cs
@@ -0,0 +1,14 @@
+﻿namespace MembersApp.Web.Views.Member
+{
+    public class MembersVM
+    {
+        public MemberVM[] MembersVMs { get; set; } = null!;
+
+        public class MemberVM
+        {
+            //public required int Id { get; set; }
+            public required string Name { get; set; }
+            public string City { get; set; }
+        }
+    }
+}
