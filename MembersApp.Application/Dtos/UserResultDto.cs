8b26a7ffbd5f422840139edc3d7f190085bc3d2e
Nowaxial <Nowaxial@users.noreply.github.com>
2025-06-10 10:00:01 +0200

    added UserResultsDto

---
+++ MembersApp.Application/Dtos/UserResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembersApp.Application.Dtos
+{
+    public record UserResultDto(string? ErrorMessage)
+    {
+        public bool Succeeded => ErrorMessage == null;
+    }
+}
