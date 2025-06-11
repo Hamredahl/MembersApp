30f4b75720385bdd546d4d3fa982119faa496966
Iryna <iryna.chorna@hotmail.com>
2025-06-11 14:04:54 +0200

    Added logic to Terminal project

--- MembersApp.Web/Views/Controllers/AccountController.cs
+++ MembersApp.Web/Views/Controllers/AccountController.cs
@@ -4,7 +4,7 @@ using MembersApp.Web.Views.Account;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MembersApp.Web.Controllers
+namespace MembersApp.Web.Views.Controllers
 {
     public class AccountController(IUserService userService) : Controller
     {
